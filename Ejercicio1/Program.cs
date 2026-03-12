2a5d2053ef0314397a4994ec6edb512e1391fb23
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 16:47:36 -0600

    Ejercicio1

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,1 +1,10 @@
 ﻿//Crear una función que reciba dos números y devuelva la suma de ambos.
+Console.WriteLine("Ingrese un numero");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese otro numero");
+int numero2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"La suma es:{suma(numero1,numero2)}");
+double suma(double a, double b)
+{
+    return a + b;
+}
