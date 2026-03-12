2ed091e07ff149af1a4cc9a50f2f902744396edd
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 17:51:33 -0600

    ejercicio7

---
+++ Ejercicio6/Program.cs
@@ -0,0 +1,9 @@
+﻿//Crear una función que reciba un número en grados Celsius y devuelva el valor convertido a Fahrenheit.
+//formula es: f=(c*9/5)+32
+Console.WriteLine("Ingrese los grados ");
+double grados = double.Parse(Console.ReadLine());
+Console.WriteLine($"Los grados fahrenheit son:{convertir(grados)}");
+double convertir (double a)
+{
+    return (a * 9 / 5)+32;
+}
