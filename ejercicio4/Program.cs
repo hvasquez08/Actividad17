f590b023af23bb39933ad13eb6de9a94481a592d
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 16:56:02 -0600

    ejercicio4

---
+++ ejercicio4/Program.cs
@@ -0,0 +1,9 @@
+﻿//Crear una función que reciba el radio de un círculo y devuelva el área del círculo.
+//la funcion es pi x radio al cuadrado oks
+Console.WriteLine("Ingrese el radio");
+double radio = double.Parse(Console.ReadLine());
+Console.WriteLine($"el radio es{area(radio)}");
+double area (double a)
+{
+    return (a * a)*(3.14);
+}
