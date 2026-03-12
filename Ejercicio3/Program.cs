038e7bd79a768a8e56ce38df52b79eed44564258
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 16:51:32 -0600

    ejercicio3

---
+++ Ejercicio3/Program.cs
@@ -0,0 +1,8 @@
+﻿//Crear una función que reciba un número y devuelva su doble.
+Console.WriteLine("Ingrese un numero");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"el doble es:{cuadrado(numero1)}");
+double cuadrado(double a)
+{
+    return a * 2;
+}
