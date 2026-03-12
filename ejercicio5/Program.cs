a6ffb8ea0b6a89860227c6dc27324f2f4ecdc520
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 17:03:07 -0600

    Ejercicio5

---
+++ ejercicio5/Program.cs
@@ -0,0 +1,11 @@
+﻿//Crear una función que reciba la base y la altura de un rectángulo y devuelva su área.
+//el area de un rectangulo es base x altura
+Console.WriteLine("Ingrese la base");
+double Base = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la altura");
+double Altura = double.Parse(Console.ReadLine());
+Console.WriteLine($"el area es:{Area(Altura,Base)}");
+double Area(double a, double b)
+{
+    return a * b;
+}
