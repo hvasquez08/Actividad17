aac229e87b24d03c6b8dcb59ebe4987e2a9b5a00
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 16:50:12 -0600

    ejercicio2

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Crear una función que reciba un número y devuelva su cuadrado.
+Console.WriteLine("Ingrese un numero");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"el cuadrado es:{cuadrado(numero1)}");
+double cuadrado(double a)
+{
+    return a * a;
+}
