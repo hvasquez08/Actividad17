247ccfb0dbbd428e9361be244a7b06bd75b65dd1
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:21:30 -0600

    ejercicio16

---
+++ ejercicio16/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese la cantidad de minutos:");
+int minutos = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Equivalente en horas: {ConvertirMinutosAHoras(minutos)}");
+
+double ConvertirMinutosAHoras(int minutos)
+{
+    return minutos / 60.0;
+}
