44398e2806b6ab3fe19a1270bdb53a7ffcb18a98
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:31:59 -0600

    ejercicio21

---
+++ ejercicio21/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese su nombre:");
+string nombre = Console.ReadLine();
+
+Bienvenida(nombre);
+
+void Bienvenida(string usuario)
+{
+    Console.WriteLine($"Bienvenido, {usuario}");
+}
