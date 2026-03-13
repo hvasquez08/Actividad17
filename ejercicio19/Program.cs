f4e8dc5bbaa434482dc2c2d6e729f4f5fdaae228
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:26:20 -0600

    ejercicio19

---
+++ ejercicio19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ingrese el nombre del estudiante:");
+string nombre = Console.ReadLine();
+
+Console.WriteLine($"El promedio final de {nombre} es: {PromedioFinal(nombre)}");
+
+double PromedioFinal(string nombre)
+{
+    double suma = 0;
+    int cantidad = 3; // tres calificaciones
+
+    for (int i = 1; i <= cantidad; i++)
+    {
+        Console.WriteLine($"Ingrese la calificación {i}:");
+        double nota = double.Parse(Console.ReadLine());
+        suma += nota;
+    }
+
+    return suma / cantidad;
+}
