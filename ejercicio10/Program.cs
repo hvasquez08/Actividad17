6983f7250eb215ed237d074e383ce2f9b12d6da6
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 18:57:23 -0600

    ejercicio10

---
+++ ejercicio10/Program.cs
@@ -0,0 +1,9 @@
+﻿//Crear una función que reciba un número y devuelva el cubo del número.
+//Crear una función que reciba un número y devuelva su cuadrado.
+Console.WriteLine("Ingrese un numero");
+int numero1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"el cuadrado es:{cuadrado(numero1)}");
+double cuadrado(double a)
+{
+    return a * a*a;
+}
