26a4fa3c6dd4e545838bfab4113b50a6e7ca613b
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:19:18 -0600

    ejercicio13

---
+++ ejercicio13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Ingrese el precio del primer producto");
+double precio1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el precio del segundo producto");
+double precio2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el precio del tercer producto");
+double precio3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"El total de la compra es: {TotalCompra(precio1, precio2, precio3)}");
+
+double TotalCompra(double a, double b, double c)
+{
+    return a + b + c;
+}
