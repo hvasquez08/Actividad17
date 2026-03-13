897c2e479abf373fd9f730e33ea2b50982995502
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 18:54:40 -0600

    ejercicio9

---
+++ ejercicio9/Program.cs
@@ -0,0 +1,10 @@
+﻿//Crear una función que reciba el salario por hora y las horas trabajadas y devuelva el salario total.
+Console.WriteLine("Ingrese las horas trabajadas");
+double horas = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el precio por hora");
+double precio = double.Parse(Console.ReadLine());
+Console.WriteLine($" el precio por hora es{Salario(horas,precio)}");
+double Salario(double a,double b)
+{
+    return a * b;
+}
