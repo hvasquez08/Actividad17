c42c1bcdb5dc06c81ce6d0fb4316305eaede925b
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:52:05 -0600

    ejercicio27

---
+++ ejercicio27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ingrese el precio del producto:");
+double precio = double.Parse(Console.ReadLine());
+
+MostrarPrecioConDescuento(precio);
+
+void MostrarPrecioConDescuento(double precio)
+{
+    double descuento = precio * 0.10;
+    double precioFinal = precio - descuento;
+    Console.WriteLine($"El precio con un 10% de descuento es: {precioFinal}");
+}
