e2c9e81ee0879eb78fc1fbe1da2fc046caa2f9c7
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:46:52 -0600

    ejercicio22

---
+++ ejercicio22/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese su nombre:");
+string nombre = Console.ReadLine();
+
+SaludoPersonalizado(nombre);
+
+void SaludoPersonalizado(string persona)
+{
+    Console.WriteLine($"hola, {persona} Espero que te la pases bien hoy xd.");
+}
