e893ef1fc1203421d0e2462cd583b99d1f5b0054
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:49:43 -0600

    ejercicio24

---
+++ ejercicio24/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ingrese el radio del círculo:");
+double radio = double.Parse(Console.ReadLine());
+
+MostrarAreaCirculo(radio);
+
+void MostrarAreaCirculo(double r)
+{
+    double area = 3.14 * r * r;
+    Console.WriteLine($"El área del círculo con radio {r} es: {area}");
+}
