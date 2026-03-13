4aebc1119694e6ada30bf73dbdaafdbb28f200fe
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:52:43 -0600

    ejercicio28

---
+++ ejercicio28/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Ingrese el nombre del estudiante:");
+string nombre = Console.ReadLine();
+
+MostrarPromedio(nombre);
+
+void MostrarPromedio(string estudiante)
+{
+    double suma = 0;
+    int cantidad = 3;
+
+    for (int i = 1; i <= cantidad; i++)
+    {
+        Console.WriteLine($"Ingrese la calificación {i}:");
+        double nota = double.Parse(Console.ReadLine());
+        suma += nota;
+    }
+
+    double promedio = suma / cantidad;
+    Console.WriteLine($"{estudiante}, su promedio es: {promedio}");
+}
