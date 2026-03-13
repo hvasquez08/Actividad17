47b66ea5fc65acf9b2be0dfe909e62128fdc22b0
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:53:24 -0600

    ejercicio29

---
+++ ejercicio29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese el número de estudiantes en el curso:");
+int cantidad = int.Parse(Console.ReadLine());
+
+MostrarCantidadEstudiantes(cantidad);
+
+void MostrarCantidadEstudiantes(int numero)
+{
+    Console.WriteLine($"En la clase hay {numero} estudiantes.");
+}
