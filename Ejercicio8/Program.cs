b6d30283f3ab22429231e37b20c2c0ebc6e1c4ff
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 18:49:23 -0600

    ejercicio8

---
+++ Ejercicio8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Crear una función que reciba tres números y devuelva el promedio de los tres.
+//le meti un for xq me aburro 
+double suma = 0;
+for (int i = 1; i<=3; i++)
+{
+    Console.WriteLine("Ingrese un numero");
+    double numero = double.Parse(Console.ReadLine());
+    suma += numero;
+
+}
+Console.WriteLine($"el promedio es{promedio(suma)}");
+double promedio(double a)
+{
+    return a / 3;
+}
