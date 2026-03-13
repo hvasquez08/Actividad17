eac486668c45def75612e1d53715495e5e6360e6
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:06:31 -0600

    ejercicio12

---
+++ ejercicio12/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Ingrese su edad");
+int edad = int.Parse(Console.ReadLine());
+
+Console.WriteLine(MostrarEdad(edad));
+
+string MostrarEdad(int edad)
+{
+    return $"La edad ingresada es: {edad}";
+}
