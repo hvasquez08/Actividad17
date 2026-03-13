13ba1c15ef3196457beee7f53a8f6cda46e5bfa5
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:22:09 -0600

    ejercicio17

---
+++ ejercicio17/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ingrese la cantidad de estudiantes:");
+int cantidad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"El promedio del grupo es: {PromedioGrupo(cantidad)}");
+
+double PromedioGrupo(int cantidad)
+{
+    double suma = 0;
+    for (int i = 1; i <= cantidad; i++)
+    {
+        Console.WriteLine($"Ingrese la nota del estudiante {i}:");
+        double nota = double.Parse(Console.ReadLine());
+        suma += nota;
+    }
+    return suma / cantidad;
+}
