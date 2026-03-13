181648dbf966ed1542b9d334a0b6eec2e55e24b7
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:30:40 -0600

    ejercicio20

---
+++ ejercicio20/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingrese la cantidad de días trabajados:");
+int dias = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el pago diario:");
+double pagoDiario = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"El salario total es: {SalarioTotal(dias, pagoDiario)}");
+
+double SalarioTotal(int dias, double pagoDiario)
+{
+    return dias * pagoDiario;
+}
