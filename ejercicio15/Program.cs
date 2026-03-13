0278e9ff879db1852215bf95039a24661110880e
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:20:48 -0600

    ejercicio15

---
+++ ejercicio15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingrese la base del triángulo:");
+double baseTriangulo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la altura del triángulo:");
+double alturaTriangulo = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"El área del triángulo es: {AreaTriangulo(baseTriangulo, alturaTriangulo)}");
+
+double AreaTriangulo(double b, double h)
+{
+    return (b * h) / 2;
+}
