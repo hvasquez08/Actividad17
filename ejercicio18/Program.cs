41df392c4c7c0b8e42528d1ba69f86fd12b7ccdf
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:24:21 -0600

    ejercicio18

---
+++ ejercicio18/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ingrese la cantidad de productos:");
+int cantidad = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"El total de la factura es: {TotalFactura(cantidad)}");
+
+double TotalFactura(int cantidad)
+{
+    double total = 0;
+    for (int i = 1; i <= cantidad; i++)
+    {
+        Console.WriteLine($"Ingrese el precio del producto {i}:");
+        double precio = double.Parse(Console.ReadLine());
+        total += precio;
+    }
+    return total;
+}
