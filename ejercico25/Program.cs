b0a7a052d55329810428f5d83090a30231fc88a0
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:50:45 -0600

    ejercicio25

---
+++ ejercico25/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ingrese la base del rectángulo:");
+double baseRectangulo = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la altura del rectángulo:");
+double alturaRectangulo = double.Parse(Console.ReadLine());
+
+MostrarAreaRectangulo(baseRectangulo, alturaRectangulo);
+
+void MostrarAreaRectangulo(double b, double h)
+{
+    double area = b * h;
+    Console.WriteLine($"El área del rectángulo con base {b} y altura {h} es: {area}");
+}
