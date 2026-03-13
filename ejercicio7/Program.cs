0c632e4d94c3e0a48f3bb4a15a8c329004365a61
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 18:03:48 -0600

    ejercicio7

---
+++ ejercicio7/Program.cs
@@ -0,0 +1,10 @@
+﻿//Crear una función que reciba el precio de un producto y el porcentaje de descuento y devuelva el precio final.
+Console.WriteLine("Ingrese el precio del producto");
+double precio = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el descuento");
+double descuento = double.Parse(Console.ReadLine());
+Console.WriteLine($"el precio final es:{PrecioFinal(precio,descuento)}");
+double PrecioFinal(double a, double b)
+{
+    return (a)-((a) * (b / 100));
+}
