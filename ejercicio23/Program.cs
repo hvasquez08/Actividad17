07e15fbc759b7e6338c0e85bad4cc33004b7fb25
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:47:49 -0600

    ejercicio22

---
+++ ejercicio23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Ingrese el primer número:");
+double numero1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número:");
+double numero2 = double.Parse(Console.ReadLine());
+
+MostrarSuma(numero1, numero2);
+
+void MostrarSuma(double a, double b)
+{
+    Console.WriteLine($"La suma de {a} + {b} es: {a + b}");
+}
