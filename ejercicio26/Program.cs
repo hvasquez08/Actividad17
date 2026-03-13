6226ffef81cc7de48023b4cdc1f3231d666e007b
Herbert Vasquez <Herbertzapeta55@gmail.com>
2026-03-12 19:51:25 -0600

    ejercicio26

---
+++ ejercicio26/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ingrese su edad:");
+int edad = int.Parse(Console.ReadLine());
+
+MostrarAnioNacimiento(edad);
+
+void MostrarAnioNacimiento(int edad)
+{
+    int anioActual = DateTime.Now.Year;
+    int anioNacimiento = anioActual - edad;
+    Console.WriteLine($"Su año aproximado de nacimiento es: {anioNacimiento}");
+}
